d08b4b6f430d3dc5492fe6c2519def8f8f9cab8d
lampard <thanh230497@gmail.com>
2019-04-24 11:37:09 +0700

    Update Enemy Class add Zombie

--- app/src/main/java/com/halloween/GameObjects/Enemies/Enemy.java
+++ app/src/main/java/com/halloween/GameObjects/Enemies/Enemy.java
@@ -1,12 +1,81 @@
 package com.halloween.GameObjects.Enemies;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.Animatable;
+
+import com.halloween.Animation;
+import com.halloween.GameObjects.GameObject;
 
 
-public class Enemy implements GameObjects{
+public class Enemy implements GameObject {
+
+    int HP;
+    public Animation currentAnimation;
+    protected Animation idleAnimation;
+    protected Animation diedAnimation;
+    protected Animation attackAnimation;
+    protected Animation defenseAnimation;
+    protected Animation hurtAnimation;
+    protected Animation ultimateAttackAnimation;
+
+
+    Point position, leftLandMark, rightLandMark;
+    Rect surroundingBox;
+
+    boolean isAlive, isMovingForward;
+
+    public Enemy(){
+        isAlive = true;
+    }
+
+    public enum State { Appear, Idle, Move , Attack, Defense , UltimateAttack, Hurt, Died} //Các state có thể có của Enemy
+    protected State currentState, previousState;
 
     @Override
-    public void draw(Canvas canvas){};
+    public void draw(Canvas canvas){}
 
     @Override
-    public void update(){};
+    public void update(){
+
+    }
+
+    public Rect getSurroundingBox(){
+        this.surroundingBox.left = position.x;
+        this.surroundingBox.top =  position.y;
+        this.surroundingBox.right = position.x + currentAnimation.frameWidth;
+        this.surroundingBox.bottom = position.y + currentAnimation.frameHeight;
+        return this.surroundingBox;
+    }
+
+    public void ChangeState(State state){
+        previousState = currentState;
+        currentState = state;
+
+        if (currentState != previousState){
+            switch (currentState){
+                case Idle:
+                    currentAnimation = idleAnimation;
+                    break;
+                case Died:
+                    currentAnimation = diedAnimation;
+                    break;
+                case Attack:
+                    currentAnimation = attackAnimation;
+                    break;
+                case Defense:
+                    currentAnimation = defenseAnimation;
+                    break;
+                case Hurt:
+                    currentAnimation = hurtAnimation;
+                    break;
+                case UltimateAttack:
+                    currentAnimation = ultimateAttackAnimation;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
 }
