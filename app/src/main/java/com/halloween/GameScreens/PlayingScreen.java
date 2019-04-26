b0a818619b56fddee90b980ec77a024ec7625807
tienbk97 <nghuuanhtien@gmail.com>
2019-04-26 13:10:28 +0700

    merged

--- app/src/main/java/com/halloween/GameScreens/PlayingScreen.java
+++ app/src/main/java/com/halloween/GameScreens/PlayingScreen.java
@@ -9,21 +9,15 @@ import android.graphics.Point;
 import android.view.MotionEvent;
 
 import com.halloween.Constants;
-<<<<<<< HEAD
 import com.halloween.GameContents.JoyStick;
-=======
->>>>>>> cf4fdfc46a248b6f28d1f26765160519d824c91b
 import com.halloween.GameObjects.MainCharacter;
 import com.halloween.R;
 
 public class PlayingScreen implements GameScreen{
     private MainCharacter mainCharacter;
-<<<<<<< HEAD
     private JoyStick joyStick;
-=======
     private Bitmap pauseButton;
     private Point pauseButtonPosition;
->>>>>>> cf4fdfc46a248b6f28d1f26765160519d824c91b
 
     public PlayingScreen() {
         this.pauseButton = BitmapFactory.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.pause_button);
@@ -47,14 +41,11 @@ public class PlayingScreen implements GameScreen{
 
     @Override
     public void draw(Canvas canvas) {
-<<<<<<< HEAD
-//        this.mainCharacter.draw(canvas);
-        this.joyStick.draw(canvas);
-=======
+
         canvas.drawColor(Color.WHITE);
         this.mainCharacter.draw(canvas);
         canvas.drawBitmap(pauseButton, pauseButtonPosition.x, pauseButtonPosition.y, new Paint());
->>>>>>> cf4fdfc46a248b6f28d1f26765160519d824c91b
+        this.joyStick.draw(canvas);
     }
 
     @Override
@@ -66,10 +57,9 @@ public class PlayingScreen implements GameScreen{
     public void receiveTouch(MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
-<<<<<<< HEAD
         if (joyStick.isInRange(x, y)) {
             joyStick.updatePosition(x, y);
-=======
+        }
         switch(event.getAction())
         {
             case MotionEvent.ACTION_UP:
@@ -77,7 +67,6 @@ public class PlayingScreen implements GameScreen{
                     Constants.CURRENT_GAME_STATE = Constants.GAME_STATE.PAUSE;
                 }
                 break;
->>>>>>> cf4fdfc46a248b6f28d1f26765160519d824c91b
         }
     }
 
