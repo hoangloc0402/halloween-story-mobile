7f4d982ae86a1214a57d46b7a1ad59d14e6f300b
tienbk97 <nghuuanhtien@gmail.com>
2019-04-28 12:51:19 +0700

    state constant

--- app/src/main/java/com/halloween/Constants.java
+++ app/src/main/java/com/halloween/Constants.java
@@ -8,4 +8,9 @@ public class Constants {
     public static Context CURRENT_CONTEXT;
     public static enum GAME_STATE {MAIN_MENU, PLAY, PAUSE, GAME_OVER}
     public static GAME_STATE CURRENT_GAME_STATE = GAME_STATE.MAIN_MENU;
+    public static GAME_STATE PREVIOUS_GAME_STATE;
+    public static enum JOYSTICK_STATE {LEFT, RIGHT, MIDDLE};
+    public static JOYSTICK_STATE CURRENT_JOYSTICK_STATE = JOYSTICK_STATE.MIDDLE;
+    public static boolean JOYSTICK_ATK_STATTE = false;
+    public static boolean JOYSTICK_JUMP_STATTE = false;
 }
