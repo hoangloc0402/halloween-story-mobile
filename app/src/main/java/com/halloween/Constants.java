b3fcf90c90f0964bd8a34b4abfe8ab5437a872b1
lampard <thanh230497@gmail.com>
2019-04-29 10:08:57 +0700

    Update Constants

--- app/src/main/java/com/halloween/Constants.java
+++ app/src/main/java/com/halloween/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     public final static int ZOMBIE_DAMAGE = 20;
     public final static int ZOMBIE_STARTING_HP = 50;
     public final static int ZOMBIE_POINT = 100;
-    public final static float ZOMBIE_V_X = 0.8f;
+    public final static int ZOMBIE_V_X = 2;
 
     public static final float MAIN_CHARACTER_V_X = 5f;
     public static final float MAIN_CHARACTER_V_Y = 21f;
