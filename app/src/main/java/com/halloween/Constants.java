9df43861e6842247f1babaf566396d4dbc6476dd
tienbk97 <nghuuanhtien@gmail.com>
2019-05-05 11:30:16 +0700

    overloading stuff

--- app/src/main/java/com/halloween/Constants.java
+++ app/src/main/java/com/halloween/Constants.java
@@ -54,8 +54,12 @@ public class Constants {
 
     public static final int backgroundMapAssetHeight = 578;
     public static final int backgroundBossMapAssetHeight = 780;
-    public static float getRelativeXPosition(float x, GAME_STATE game_state) {
-        switch (game_state) {
+    public static float getRelativeXPosition(float x, GAME_STATE game_state)  {
+        return getRelativeXPosition(x);
+    }
+
+    public static float getRelativeXPosition(float x) {
+        switch (CURRENT_GAME_STATE) {
             case PLAY:
                 return (x - BACKGROUND_X_AXIS) * SCREEN_WIDTH / (backgroundMapAssetHeight * SCREEN_WIDTH / SCREEN_HEIGHT);
             case BOSS:
@@ -65,8 +69,8 @@ public class Constants {
         }
     }
 
-    public static float getAbsoluteXLength(float x, GAME_STATE game_state) {
-        switch (game_state) {
+    public static float getAbsoluteXLength(float x) {
+        switch (CURRENT_GAME_STATE) {
             case PLAY:
                 return x * (backgroundMapAssetHeight * SCREEN_WIDTH / SCREEN_HEIGHT) / SCREEN_WIDTH;
             case BOSS:
@@ -75,8 +79,11 @@ public class Constants {
                 return 0;
         }
     }
-
     public static boolean isInScreenRange(float x, float offset, GAME_STATE game_state) {
+        return isInScreenRange(x, offset);
+    }
+    public static boolean isInScreenRange(float x, float offset) {
+        GAME_STATE game_state = CURRENT_GAME_STATE;
         float left = getRelativeXPosition(x, game_state);
         float right = getRelativeXPosition(x + offset, game_state);
         switch (game_state) {
